49cbc05fa7d559b8753900017379a698c9c78550
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-30 01:16:16 +0330

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,13 @@
-from threading import Thread
+from checker import checker_app
 from uploader import uploader_app
-from checker import app as checker_app
+from threading import Thread
 
-if __name__ == '__main__':
-    Thread(target=lambda: uploader_app.run(host='0.0.0.0', port=5000)).start()
-    Thread(target=lambda: checker_app.run(host='0.0.0.0', port=5001)).start()
+def run_checker():
+    checker_app.run(host="0.0.0.0", port=5000)
+
+def run_uploader():
+    uploader_app.run(host="0.0.0.0", port=5001)
+
+if __name__ == "__main__":
+    Thread(target=run_checker).start()
+    Thread(target=run_uploader).start()
